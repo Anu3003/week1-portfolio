9d0cb41ecc7744cb6efc807fa5ab19b2ba3fe8cf
Veluvarthi Venkata Anuradha <anuradhaveluvarthivenkata@gmail.com>
2026-01-04 11:20:12 +0530

    Week 1: Personal Portfolio Website complete

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+
+    // Animate burger lines
+    burger.classList.toggle('toggle');
+});
